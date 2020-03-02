06814ee3a2959234cf1bc77d72ee691c37605874
Eric Settels and Michelle Morin <michelle@>
2020-03-02 13:30:15 -0800

    complete red, green, refacotr workflow for three tests

---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,47 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTests
+  {
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      // Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      // Act
+      string result = newItem.Description;
+
+      // Assert
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+  }
+}
---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,12 @@
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public string Description { get; set; }
+
+    public Item(string description)
+    {
+      Description = description;
+    }
+  }
+}
