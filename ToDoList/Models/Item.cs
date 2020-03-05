ac4262e1ac1566d1e60b22067d5dffa4d2c36ee1
Michelle Morin <morin2m@gmail.com>
2020-03-04 17:54:49 -0800

    add overloaded constructor

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,12 +5,19 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
+    public string Priority { get; set; }
     private static List<Item> _instances = new List<Item> {};
 
     public Item(string description)
     {
       Description = description;
       _instances.Add(this);
+    }
+
+    public Item(string description, int priority)
+      : this(description)
+    {
+      Priority = priority;
     }
 
     public static List<Item> GetAll()
