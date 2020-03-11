17356243672a59102a0a9f4fecbe1d0e6627aaaf
Michelle Morin <morin2m@gmail.com>
2020-03-10 20:17:44 -0700

    add tests for new Id property and Find method

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,6 +5,7 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
+    public int Id { get; }
     public string Priority { get; set; }
     private static List<Item> _instances = new List<Item> {};
 
@@ -12,6 +13,7 @@ namespace ToDoList.Models
     {
       Description = description;
       _instances.Add(this);
+      Id = _instances.Count;
     }
 
     public Item(string description, string priority)
@@ -28,6 +30,11 @@ namespace ToDoList.Models
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
   }
 }
