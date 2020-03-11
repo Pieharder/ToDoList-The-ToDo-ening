f80af2af0e940afbfcb4cc2c19eed807e6893470
Geoff Goetz and Michelle Morin <michelle@>
2020-03-11 15:56:13 -0700

    update Show route

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -6,24 +6,21 @@ namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-    [HttpGet("/items")]
-    public ActionResult Index()
-    {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
-
     [HttpGet("/items/new")]
     public ActionResult New()
     {
       return View();
     }
 
-    [HttpGet("/items/{id}")]
-    public ActionResult Show(int id)
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
     {
-      Item foundItem = Item.Find(id);
-      return View(foundItem);
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
     }
 
     [HttpPost("/items")]
